d6ebfa3d0728f575c6cc092ff23b44f4f6da6b95
florisvdh <floris.vanderhaeghe@inbo.be>
2019-04-04 20:28:34 +0200

    Docu namelist: clarification & language improvement

--- R/textdata.R
+++ R/textdata.R
@@ -1,31 +1,37 @@
-#' Documentation of datafile 'namelist'
+#' Documentation of included data source 'namelist'
 #'
-#' A datafile in the
+#' 'namelist' is a data source in the
 #' \href{https://inbo.github.io/git2rdata/index.html}{vc-format} which provides
 #' names and (optionally) shortnames for IDs/codes used in other data sources.
 #' Multiple languages are supported.
 #'
-#' @format A vc-formatted datafile with many rows and 4 variables:
+#' @format A vc-formatted data source. As such, it corresponds to
+#' a dataframe \code{namelist} with many rows and 4 variables:
 #' \describe{
 #'   \item{code}{A code used elsewhere.}
 #'   \item{lang}{An
 #'   \href{https://www.w3.org/International/articles/language-tags/index.en}{IETF BCP
-#'   47 language tag}, such as \code{"en"} or \code{"nl"}, to identify the language of the
-#'   corresponding value of variables \code{name} and \code{shortname}.}
+#'   47 language tag}, such as \code{"en"} or \code{"nl"}, to identify
+#'   the language of \code{name} and \code{shortname}.}
 #'   \item{name}{The name corresponding to \code{code} and \code{lang}.}
 #'   \item{shortname}{Optionally, a shorter variant of \code{name}.} }
-#'
-#' @source
-#'
-#'   \code{inst/textdata/namelist.tsv}
-#'
-#'   \code{inst/textdata/namelist.yml}
 #'
 #' @section Typical way of loading:
 #'
 #'   \code{read_namelist()}
 #'
 #'   \code{read_namelist(lang = "nl")}
+#'
+#' @section Corresponding datafiles in the installed package:
+#'
+#'   \code{inst/textdata/namelist.tsv}
+#'
+#'   \code{inst/textdata/namelist.yml}
+#'
+#' @source
+#'
+#' The 'namelist' data source has got its contents from the sources of
+#' several other n2khab-referencelists (see the source of those).
 #'
 #' @seealso \code{\link{read_namelist}}
 #'
