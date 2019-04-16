398e0d77c014edc17c844c2cdeaa0258106afedf
florisvdh <floris.vanderhaeghe@inbo.be>
2019-04-16 20:57:29 +0200

    n2khabutils function: read_scheme_types()

--- R/read_textdata.R
+++ R/read_textdata.R
@@ -663,3 +663,170 @@ read_schemes <-
 
 
 
+#' Return the 'scheme_types' data source as a tibble with names & shortnames
+#'
+#' Returns the included data source \code{\link{scheme_types}} as a
+#' \code{\link[tibble:tbl_df-class]{tibble}}.
+#' Names and shortnames from \code{\link{namelist}} are added,
+#' in English by default.
+#'
+#' \code{\link{scheme_types}} is a data source in the
+#' \href{https://inbo.github.io/git2rdata/index.html}{vc-format} which lists
+#' the types (using the type-code from \code{\link{types}}) that belong to
+#' each N2KHAB (monitoring or research) scheme (using the scheme-code from
+#' \code{\link{schemes}}).
+#' It also defines typegroup memberships of the types within specific schemes,
+#' if applicable.
+#'
+#' \code{read_scheme_types()} reads the \code{\link{scheme_types}} data
+#' source, adds names + shortnames and returns it as a
+#' \code{\link[tibble:tbl_df-class]{tibble}}.
+#' A tibble is a dataframe that makes working in the tidyverse a little
+#' \href{https://r4ds.had.co.nz/tibbles.html}{easier}.
+#' By default, the data version delivered with the package is used and English
+#' names (\code{lang = "en"}) are returned for scheme, programme,
+#' scheme attributes, type, typeclass and tags of scheme and type.
+#'
+#' @param path Location of the data sources \code{scheme_types},
+#' \code{schemes}, \code{types} and \code{namelist}.
+#' The default is to use the location of the data sources as delivered by
+#' the installed package.
+#' @param file The filename of the \code{scheme_types} data source, without extension.
+#' The default is to use the file delivered by the installed package.
+#'
+#' @inheritParams read_types
+#' @inheritParams read_env_pressures
+#'
+#' @return
+#' The \code{scheme_types} dataframe as a \code{\link[tibble:tbl_df-class]{tibble}},
+#' with names & shortnames added for scheme, programme,
+#' scheme attributes, type, typeclass and tags of scheme and type, all
+#' according to the \code{lang} argument.
+#' The tibble has 44 variables.
+#' See \code{\link{scheme_types}} for documentation of the data-source's contents.
+#' See \code{\link{namelist}} for the link between codes or other identifiers
+#' and the corresponding names (and shortnames).
+#'
+#' The added names and shortnames are represented by the following variables:
+#' \itemize{
+#'   \item \code{scheme_name}
+#'   \item \code{scheme_shortname}
+#'   \item \code{programme_name}
+#'   \item \code{attribute_1_name}
+#'   \item \code{attribute_1_shortname}
+#'   \item \code{attribute_2_name}
+#'   \item \code{attribute_2_shortname}
+#'   \item \code{attribute_3_name}
+#'   \item \code{attribute_3_shortname}
+#'   \item \code{schemetag_1_name}
+#'   \item \code{schemetag_1_shortname}
+#'   \item \code{schemetag_2_name}
+#'   \item \code{schemetag_2_shortname}
+#'   \item \code{schemetag_3_name}
+#'   \item \code{schemetag_3_shortname}
+#'   \item \code{type_name}
+#'   \item \code{type_shortname}
+#'   \item \code{typeclass_name}
+#'   \item \code{typetag_1_name}
+#'   \item \code{typetag_1_shortname}
+#'   \item \code{typetag_2_name}
+#'   \item \code{typetag_2_shortname}
+#'   \item \code{typetag_3_name}
+#'   \item \code{typetag_3_shortname}
+#' }
+#'
+#' The added names and shortnames for scheme, programme, typeclass and
+#' attributes are \emph{factors} with their level order according to that of
+#' the scheme, programme, typeclass or attribute variable.
+#'
+#' @section Recommended usage:
+#'
+#'   \code{read_scheme_types()}
+#'
+#'   \code{read_scheme_types(lang = "nl")}
+#'
+#' @seealso
+#' \code{\link{scheme_types}}
+#'
+#' @family reading functions for n2khab-referencelists
+#'
+#' @examples
+#' \dontrun{
+#' read_scheme_types()
+#' read_scheme_types(lang = "nl")
+#' }
+#'
+#' @export
+#' @importFrom git2rdata read_vc
+#' @importFrom dplyr
+#' %>%
+#' select
+#' mutate
+#' left_join
+#' as_tibble
+#' contains
+#' pull
+#' @importFrom tidyr gather spread
+#' @importFrom stringr str_c
+#' @importFrom rlang .data
+read_scheme_types <- function(path = pkgdatasource_path("textdata/scheme_types", ".tsv"),
+                              file = "scheme_types",
+                              file_namelist = "namelist",
+                              lang = "en") {
+
+    schemes <-
+        read_schemes(path = path,
+                     file = "schemes",
+                     file_namelist = file_namelist,
+                     lang = lang) %>%
+        gather(key = "key",
+               value = "value",
+               contains("tag")) %>%
+        mutate(key = str_c("scheme", .data$key)) %>%
+        spread(key = .data$key, value = .data$value)
+
+    types <-
+        read_types(path = path,
+                   file = "types",
+                   file_namelist = file_namelist,
+                   lang = lang) %>%
+        gather(key = "key",
+               value = "value",
+               contains("tag")) %>%
+        mutate(key = str_c("type", .data$key)) %>%
+        spread(key = .data$key, value = .data$value)
+
+    namelist <-
+        read_namelist(path = path,
+                      file = file_namelist,
+                      lang = lang) %>%
+        select(.data$code,
+               .data$name,
+               .data$shortname)
+
+    read_vc(file = file, root = path) %>%
+        left_join(schemes,
+                  by = "scheme") %>%
+        mutate(type = .data$type %>% as.character) %>%
+        left_join(types,
+                  by = "type") %>%
+        mutate(type = .data$type %>%
+                        factor(levels =
+                                   read_vc(file = file, root = path) %>%
+                                   pull(.data$type) %>%
+                                   levels
+                                   )) %>%
+        mutate(typegroup_name = namelist_factor(.data$typegroup,
+                                                codelist = namelist),
+               typegroup_shortname = namelist_factor(.data$typegroup,
+                                                     "shortname",
+                                                     codelist = namelist)) %>%
+        as_tibble
+
+}
+
+
+
+
+
+
