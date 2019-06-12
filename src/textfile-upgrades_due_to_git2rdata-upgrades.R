f9ecdde246ec00e3e3f5680a004433beccdeee51
florisvdh <floris.vanderhaeghe@inbo.be>
2019-06-12 20:34:42 +0200

    Splitting n2khab-inputs: exclude src from data-upgrade script

--- src/textfile-upgrades_due_to_git2rdata-upgrades.R
+++ src/textfile-upgrades_due_to_git2rdata-upgrades.R
@@ -40,7 +40,5 @@ git2rdata::upgrade_data(path = "inst/textdata")
 # Previous state of git2rdata: commit df1ba32
 # Conversion to new state of git2rdata: commit 796ba98 (tag 0.0.5)
 
-git2rdata::upgrade_data(path = "src/generate_textdata/rawraw_data")
 git2rdata::upgrade_data(path = "inst/textdata")
-git2rdata::upgrade_data(path = "src/generate_habitatmap_stdized/habmap_correction")
 
