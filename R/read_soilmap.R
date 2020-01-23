8ead1fa8c7d77cff3ab0ed5a1405b9d1cfda7573
florisvdh <floris.vanderhaeghe@inbo.be>
2020-01-23 12:29:22 +0100

    read_soilmap() docu: add family

--- R/read_soilmap.R
+++ R/read_soilmap.R
@@ -172,6 +172,8 @@
 #'
 #' (*) Included in the `soilmap_simple` data source.
 #'
+#' @family functions returning important environmental data sets
+#'
 #' @references
 #' - Ampe C. (2013). Databank aardewerk Vlaanderen 2010.
 #' Omzetten (zeer) oude legende bodemkartering naar legende bodemkaart
