c490a0861e2fee9f03c5b3ecce9b076ae78b245d
florisvdh <floris.vanderhaeghe@inbo.be>
2021-12-15 15:23:11 +0100

    Add new function: read_shallowgroundwater

---
+++ R/read_shallowgroundwater.R
@@ -0,0 +1,45 @@
+#' Return the data source \code{shallowgroundwater} as an \code{sf}
+#' multipolygon layer
+#'
+#' Returns the raw data source \code{shallowgroundwater}
+#' as an \code{sf} multipolygon
+#' layer.
+#' The coordinate reference system is 'Belge 72 / Belgian Lambert 72'
+#' (EPSG-code \href{https://epsg.io/31370}{31370}).
+#'
+#' The R code for creating the \code{shallowgroundwater} data source,
+#' starting from a manually composed layer, can be found in the
+#' \href{https://github.com/inbo/n2khab-preprocessing}{n2khab-preprocessing}
+#' repository.
+#'
+#' @inheritParams read_habitatmap_stdized
+#'
+#' @return
+#' A Simple feature collection of geometry type \code{MULTIPOLYGON}.
+#'
+#' @examples
+#' \dontrun{
+#' shallowgroundwater <- read_shallowgroundwater()
+#' shallowgroundwater
+#' }
+#'
+#' @importFrom assertthat
+#' assert_that
+#' @importFrom sf
+#' read_sf
+#' @export
+read_shallowgroundwater <-
+    function(file = file.path(fileman_up("n2khab_data"),
+                              "10_raw/shallowgroundwater/shallowgroundwater.gpkg")){
+
+        assert_that(file.exists(file))
+
+        shallowgroundwater <-
+            suppressWarnings(
+                read_sf(file,
+                        crs = 31370)
+            )
+
+        return(shallowgroundwater)
+
+    }
