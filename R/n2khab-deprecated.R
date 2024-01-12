637389967986bda1dc8f1a8c24b5704d6492bedd
florisvdh <floris.vanderhaeghe@inbo.be>
2024-01-12 10:55:04 +0100

    read_scheme*(): add advice how to anticipate conflicts

--- R/n2khab-deprecated.R
+++ R/n2khab-deprecated.R
@@ -5,7 +5,14 @@
 #' - [`read_scheme_types()`] has moved to package `n2khabmon`.
 #' Use [`n2khabmon::read_scheme_types()`] instead.
 #'
-#' @param ... arguments passed to the new function
+#' An efficient way with base R to avoid function masking and conflict warnings
+#' when attaching both `{n2khab}` and `{n2khabmon}`, regardless of the order
+#' in which they're loaded, is by specifying something as below in your
+#' script, at least _before_ loading `{n2khab}`:
+#'
+#' `conflictRules("n2khab", exclude = c("read_schemes", "read_scheme_types"))`
+#'
+#' @param ... Arguments passed to the new function.
 #'
 #' @md
 #'
