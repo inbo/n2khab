d4bcc0ecd014d1089154fdb61f41e69c1da2a15e
Hans Van Calster <hans.vancalster@inbo.be>
2025-03-13 09:39:10 +0100

    get_zenodo_versions: capitalization
    Co-authored-by: Floris Vanderhaeghe <floris.vanderhaeghe@inbo.be>

--- R/filemanagement.R
+++ R/filemanagement.R
@@ -234,7 +234,7 @@ download_zenodo <- function(doi,
 #' The function queries the Zenodo API to find the version IDs and
 #' corresponding DOIs.
 #'
-#' @param doi a doi pointer to the Cite-all-versions doi for a Zenodo archive.
+#' @param doi A DOI pointer to the Cite-all-versions DOI for a Zenodo archive.
 #' A Zenodo archive starts with '10.5281/zenodo.'
 #'
 #' @export
